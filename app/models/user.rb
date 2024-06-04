e53b638c284c66570ceb8ec124b6dc9d36e91bd4
splmloic <l2ct@protonmail.com>
2024-06-04 23:16:57 +0200

    Add Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :email, presence: true
   # Include default devise modules. Others available are:
